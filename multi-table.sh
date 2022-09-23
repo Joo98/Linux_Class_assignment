9e21b9248080a2bd24e1f4b87ccd6e993bc9a1d4
joo98 <hynciath51@gmail.com>
2022-09-23 16:59:18 +0900

    add code for error

--- multi-table.sh
+++ multi-table.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 n=$1
 m=$2
+if [ $# -ne 2 ];then
+	echo "invalid input"
+	exit 0
+fi
 
 if [ $n -le 0 ] || [ $m -le 0 ];then
-	echo "invalid input"
+	echo "invalid input, inputs should be greater than 0"
 	exit 0
 fi
 
